51494741b6a2ca2232696e577e7ffdac0b37bdb2
dharani2005 <58557415+dharani2005@users.noreply.github.com>
2024-06-03 14:13:25 +0200

    Initial commit

---
+++ src/main/java/org/example/springbootworkshop/SpringBootWorkshopApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootworkshop/SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
